b88889b2694d22a8519f8f3aa3d4916384941ed2
Boon Han <charayaphan.nakorn.boon.han@gmail.com>
2016-02-12 00:30:30 +0800

    allow dual links for pi

--- chart_magni_rpi4/installation/start_device.bash
+++ chart_magni_rpi4/installation/start_device.bash
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 sudo rfkill unblock all
-#sudo systemctl restart hostapd.service
-#sudo apt update && upgrade -y
+
+sudo nmcli device set wlx58d56e91d24f managed off
+sudo ifdown wlx58d56e91d24f
+sudo ifup wlx58d56e91d24f
+sudo ip route add 10.233.0.1/24 via 10.233.29.1
 
 source /opt/ros/kinetic/setup.bash
 #@source $HOME/deployment_ws/devel/setup.bash
