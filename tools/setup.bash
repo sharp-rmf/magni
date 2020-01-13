ce3267b57e5b7792cbac3b581a4bb29acc61f4bd
Boon Han <cnboonhan94@gmail.com>
2020-01-13 15:35:38 +0800

    add launch tool for lidar

--- tools/setup.bash
+++ tools/setup.bash
@@ -6,4 +6,12 @@ PROJECT_ROOT="$HOME/dev/magni_45"
 
 alias ros1="source /opt/ros/$ROS_DISTRO/setup.bash && source $HOME/dev/magni_45/ros1/devel/setup.bash"
 alias b1="cd $HOME/dev/magni_45/ros1 && catkin build"
+alias a="ros1 && roslaunch magni_45_rplidar dual_rplidar.launch"
 
+m3(){
+    # For use on external device visualizing the data from the magni
+    export ROS_IP=10.233.29.77
+    gnome-terminal -- bash -c "echo 'Launching Magni'; ssh magni3; exec bash"
+    export ROS_MASTER_URI=http://magni3.local:11311
+    rviz &
+}
