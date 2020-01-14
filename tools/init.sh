3da1ffba6d99732cdc819b6afb06d57054662dd1
Boon Han <cnboonhan94@gmail.com>
2020-01-14 10:10:48 +0800

    rewrite launch to remove redundancies

--- tools/init.sh
+++ tools/init.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 PROJECT_ROOT="$HOME/dev/magni_45"
+ROS_DISTRO="kinetic"
 
 # Required packages and one-time setup 
 sudo apt install picocom -y
 sudo apt install python3-vcstool -y
 sudo apt install python3-pip -y
 sudo apt install npyscreen -y
+sudo apt install ros-$ROS_DISTRO-map-server -y
 
 echo "Copying udev rules.."
 sudo cp $PROJECT_ROOT/ros1/udev/*.rules /etc/udev/rules.d
