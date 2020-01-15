170f8890aa489e3386383c1331856c4c5d1817c1
Boon Han <cnboonhan94@gmail.com>
2020-01-15 14:28:26 +0800

    live decawave update

--- ros1/src/magni_45_uwb/scripts/dwm.py
+++ ros1/src/magni_45_uwb/scripts/dwm.py
@@ -31,13 +31,13 @@ def listen(ser, pub, tf_listener):
             data = str(ser.readline())
 
             # parse data
-            print("Parsing line: " + data)
-            m = re.match(r"POS,(.*),(.*),(.*),.*", data)
+	    if data != '':
+		print("Parsing line: " + data)
+
+	    m = re.match(r"POS,(.*),(.*),(.*),.*", data)
+
             if m:
-                print("match found!")
-                print(m.group(1))
-                print(m.group(2))
-                print(m.group(3))
+                print("match found!\n")
                 last_parsed = rospy.Time.now()
             else:
                 if (rospy.Time.now() - last_parsed).to_sec() > 15:
@@ -45,41 +45,68 @@ def listen(ser, pub, tf_listener):
                     # Waited too long, something probably failed
                     start(ser)
                     last_parsed = rospy.Time.now()
-                    pass
+		continue
 
+	    # Print received decawave position
+	    x = float(m.group(1))
+	    y = float(m.group(2))
+	    z = float(m.group(3))
+	    print("{},{},{}".format(x, y, z))
+
+            # Get current robot pose
             if not rospy.is_shutdown():
                 try:
-                    (trans, rot) = tf_listener.lookupTransform('map','base_link',
-                            rospy.Time(0))
+                    (trans, rot)=tf_listener.lookupTransform('map', 'base_link',
+                                                               rospy.Time(0))
                 except(tf.LookupException,
                         tf.ConnectivityException,
                         tf.ExtrapolationException) as e:
                     print(e)
-                    pass
-                pub.publish(data)
+                    continue
+
+            msg=construct_pose_update(x, y, z, rot)
+            pub.publish(msg)
             time.sleep(0.01)
+
         except Exception as e:
             print(e)
             break
         except KeyboardInterrupt:
-            print("C")
             print("Keyboard Interrupt registered.")
             break
 
+
+def construct_pose_update(x, y, z, rot):
+    msg=PoseWithCovarianceStamped()
+    msg.header.frame_id="map"
+    msg.header.stamp=rospy.Time.now()
+    msg.pose.pose.position.x=x
+    msg.pose.pose.position.y=y
+    msg.pose.pose.position.z=0
+    msg.pose.covariance=[0.25, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.25, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+                            0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0685]
+    msg.pose.pose.orientation.x = rot[0]
+    msg.pose.pose.orientation.y = rot[1]
+    msg.pose.pose.orientation.z = rot[2]
+    msg.pose.pose.orientation.w = rot[3]
+    return msg
+
+
 if __name__ == "__main__":
-    ser = serial.Serial()
-    ser.port = '/dev/decawave'
-    ser.baudrate = 115200
-    ser.bytesize = serial.EIGHTBITS
-    ser.parity = serial.PARITY_NONE
-    ser.stopbits = serial.STOPBITS_ONE
-    ser.timeout = 1
+    ser=serial.Serial()
+    ser.port='/dev/decawave'
+    ser.baudrate=115200
+    ser.bytesize=serial.EIGHTBITS
+    ser.parity=serial.PARITY_NONE
+    ser.stopbits=serial.STOPBITS_ONE
+    ser.timeout=1
 
     # Specify topics to listen and publish to here
     # pub = rospy.Publisher('/uwb/pos', PoseWithCovarianceStamped, queue_size=10)
-    pub = rospy.Publisher('/uwb/pos', String, queue_size=10)
+    pub=rospy.Publisher(
+        '/initialpose', PoseWithCovarianceStamped, queue_size=10)
     rospy.init_node('uwb_publisher')
-    tf_listener = tf.TransformListener()
+    tf_listener=tf.TransformListener()
     try:
         start(ser)
         listen(ser, pub, tf_listener)
