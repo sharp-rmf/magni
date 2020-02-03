dfb483a3a41c037b19f0c0ee407530f0df9ec2a4
Boon Han <cnboonhan94@gmail.com>
2020-02-03 14:09:05 +0800

    update launch shortcuts

--- tools/setup.bash
+++ tools/setup.bash
@@ -6,5 +6,5 @@ PROJECT_ROOT="$HOME/dev/magni_45"
 
 alias ros1="source /opt/ros/$ROS_DISTRO/setup.bash && source $HOME/dev/magni_45/ros1/devel/setup.bash"
 alias b1="cd $HOME/dev/magni_45/ros1 && catkin build"
-alias a="ros1 && roslaunch magni_45_rplidar magni_45.launch"
+alias a="ros1 && roslaunch magni_45 magni_45.launch"
 
