db3ae1a3baa511f7fbc5ac770425f3c971d7a603
Charayaphan Nakorn Boon Han <12628669+cnboonhan94@users.noreply.github.com>
2020-07-28 16:02:07 +0800

    Update setup.bash

--- tools/setup.bash
+++ tools/setup.bash
@@ -4,7 +4,15 @@
 ROS_DISTRO="kinetic"
 PROJECT_ROOT="$HOME/dev/magni_45"
 
-alias ros1="source /opt/ros/$ROS_DISTRO/setup.bash && source $HOME/dev/magni_45/ros1/devel/setup.bash"
-alias b1="cd $HOME/dev/magni_45/ros1 && catkin build"
-alias a="ros1 && roslaunch magni_45 magni_45.launch"
+alias ros1_kinetic="source /opt/ros/kinetic/setup.bash"
+alias dp2_env_ros1="source $HOME/catkin_ws/devel/setup.bash"
+alias a='ros1_kinetic && dp2_env_ros1 && roslaunch magni_45 magni_45_single_lidar.launch&'
+alias b='ros1_kinetic && dp2_env_ros1 && roslaunch magni_45 magni_45_free_fleet_client.launch& && rostopic pub /level_name std_msgs/String "B1" -1'
+alias c='ros1_kinetic && dp2_env_ros1 && roslaunch magni_45 magni_45_uwb.launch&'
+alias x='pkill -f magni_45_single_lidar.launch && pkill -f magni_45_free_fleet_client.launch && pkill -f magni_45_uwb.launch'
+
+alias syscheck_1='sudo systemctl status magni-base.service'
+alias syscheck_2='rostopic echo /battery_state'
+alias syscheck_3='rostopic echo /joy'
+
 
