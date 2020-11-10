1577bc6a3979f53ab1b5d2182a1df7c8cc097f9b
Boon Han <charayaphan.nakorn.boon.han@gmail.com>
2020-11-10 16:52:06 +0800

    deploy roscore service

--- chart_magni_nuc/installation/setup.bash
+++ chart_magni_nuc/installation/setup.bash
@@ -179,6 +179,11 @@ echo -e "Deploying $SERVICE_NAME"
 sudo cp $SCRIPT_DIR/config/$SERVICE_NAME.service /etc/systemd/system
 sudo systemctl unmask $SERVICE_NAME.service && sudo systemctl daemon-reload && sudo systemctl enable $SERVICE_NAME.service && sudo systemctl restart $SERVICE_NAME.service
 
+SERVICE_NAME=start-roscore
+echo -e "Deploying $SERVICE_NAME"
+sudo cp $SCRIPT_DIR/config/$SERVICE_NAME.service /etc/systemd/system
+sudo systemctl unmask $SERVICE_NAME.service && sudo systemctl daemon-reload && sudo systemctl enable $SERVICE_NAME.service && sudo systemctl restart $SERVICE_NAME.service
+
 echo -e "Deploying startup script"
 cp $SCRIPT_DIR/start_device.bash $HOME
 
