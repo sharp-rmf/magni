d5cb5d06d6ba040cab72ce66a641d40167d4e3b3
Boon Han <charayaphan.nakorn.boon.han@gmail.com>
2020-11-19 10:39:51 +0800

    add dhcp

--- chart_magni_rpi4/installation/start_device.bash
+++ chart_magni_rpi4/installation/start_device.bash
@@ -6,6 +6,7 @@ sudo nmcli device set wlx58d56e91d24f managed off
 sudo ifdown wlx58d56e91d24f
 sudo ifup wlx58d56e91d24f
 sudo ip route add 10.233.0.1/24 via 10.233.29.1
+sudo dhclient wlx58d56e91d24f
 
 sudo wpa_supplicant -B -i wlx58d56e91d24f -c /etc/wpa_supplicant/wpa_supplicant.conf 
 
