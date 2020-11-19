170772afc6168610159867b46fea7c389f92a6c5
Boon Han <charayaphan.nakorn.boon.han@gmail.com>
2020-11-19 10:47:44 +0800

    fix typo in sequence:

--- chart_magni_rpi4/installation/start_device.bash
+++ chart_magni_rpi4/installation/start_device.bash
@@ -6,9 +6,9 @@ sudo nmcli device set wlx58d56e91d24f managed off
 sudo ifdown wlx58d56e91d24f
 sudo ifup wlx58d56e91d24f
 sudo ip route add 10.233.0.1/24 via 10.233.29.1
+sudo wpa_supplicant -B -i wlx58d56e91d24f -c /etc/wpa_supplicant/wpa_supplicant.conf 
 sudo dhclient wlx58d56e91d24f
 
-sudo wpa_supplicant -B -i wlx58d56e91d24f -c /etc/wpa_supplicant/wpa_supplicant.conf 
 
 source /opt/ros/kinetic/setup.bash
 source $HOME/deployment_ws/devel/setup.bash
