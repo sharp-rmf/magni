f8c3895d34132dcbf0562b40baaa5f01405aca0b
Boon Han <charayaphan.nakorn.boon.han@gmail.com>
2020-11-19 10:38:43 +0800

    add wifi

--- chart_magni_rpi4/installation/start_device.bash
+++ chart_magni_rpi4/installation/start_device.bash
@@ -7,6 +7,8 @@ sudo ifdown wlx58d56e91d24f
 sudo ifup wlx58d56e91d24f
 sudo ip route add 10.233.0.1/24 via 10.233.29.1
 
+sudo wpa_supplicant -B -i wlx58d56e91d24f -c /etc/wpa_supplicant/wpa_supplicant.conf 
+
 source /opt/ros/kinetic/setup.bash
-#@source $HOME/deployment_ws/devel/setup.bash
-#echo "Running Node"
+source $HOME/deployment_ws/devel/setup.bash
+roslaunch magni_45 magni_45.launch
