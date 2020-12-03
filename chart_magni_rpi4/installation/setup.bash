1a07fde8af330673f82e32cfce854865ad5c9282
Boon Han <charayaphan.nakorn.boon.han@gmail.com>
2020-12-03 11:39:06 +0800

    update

--- chart_magni_rpi4/installation/setup.bash
+++ chart_magni_rpi4/installation/setup.bash
@@ -180,6 +180,11 @@ echo -e "Deploying $SERVICE_NAME"
 sudo cp $SCRIPT_DIR/config/$SERVICE_NAME.service /etc/systemd/system
 sudo systemctl unmask $SERVICE_NAME.service && sudo systemctl daemon-reload && sudo systemctl enable $SERVICE_NAME.service && sudo systemctl restart $SERVICE_NAME.service
 
+SERVICE_NAME=restore-last-pose
+echo -e "Deploying $SERVICE_NAME"
+sudo cp $SCRIPT_DIR/config/$SERVICE_NAME.service /etc/systemd/system
+sudo systemctl unmask $SERVICE_NAME.service && sudo systemctl daemon-reload && sudo systemctl enable $SERVICE_NAME.service && sudo systemctl restart $SERVICE_NAME.service
+
 echo -e "Deploying startup script"
 cp $SCRIPT_DIR/start_device.bash $HOME
 cp $SCRIPT_DIR/start_free_fleet.bash $HOME
