78cc63924676a622b9d4433235149ef8d7ca0028
CHART <45197916+chartrmf@users.noreply.github.com>
2021-01-21 18:59:05 +0800

    Update start_free_fleet.bash

--- start_free_fleet.bash
+++ start_free_fleet.bash
@@ -4,7 +4,4 @@
 source /opt/ros/kinetic/setup.bash
 source /home/ubuntu/deployment_ws/devel/setup.bash
 
-export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp
-export CYCLONEDDS_URI=file:///home/ubuntu/cyclonedds.xml
-
 # roslaunch magni_45 magni_45_free_fleet_client.launch
