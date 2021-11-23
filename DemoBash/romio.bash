2dee5d6ae43c4b480b2625a9f04e6658b4496e74
kjchee <keai_jiang_chee@cgh.com.sg>
2021-11-23 16:45:47 +0800

    Updated for hybrid nav mode feature

--- DemoBash/romio.bash
+++ DemoBash/romio.bash
@@ -12,27 +12,6 @@ if [ $? != 0 ]; then
 
     # Highlight active window
     tmux set-window-option -g window-status-current-bg green
-
-    # history limit
-    tmux set -g history-limit 10000
-
-    # Set status bar
-    tmux set -g status-bg black
-    tmux set -g status-fg white 
-    
-    # to enable mouse copy/paste to system buffer
-    # to copy text, go to the any window and press this sequence
-    #     C-b [
-    # now you are in VI mode, use the arrow keys and move to the start of text to copy
-    # To select the text, press the key 'v'
-    # Continue moving using the arrow keys to highlight the text to copy
-    # To copy the text, press the 'y' key
-    # Now your highlighted text is in system clipboard, can just use ctrl-V to paste into text editor
-    # if you want to paste to another tmux buffer, press this sequence
-    #     C-b ]
-    tmux setw -g mode-keys vi
-    tmux bind-key -t vi-copy 'v' begin-selection
-    tmux bind-key -t vi-copy 'y' copy-pipe "xclip -sel clip -i"
     
     # roscore
     tmux send-keys -t tmux_run 'source /opt/ros/melodic/setup.bash && roscore' C-m
@@ -52,44 +31,18 @@ if [ $? != 0 ]; then
     tmux select-layout tiled
 
     tmux split-window -h -t tmux_run
-    tmux send-keys -t tmux_run 'sleep 20; rviz -d ~/magni_ws/src/magni/magni/config/dp3.rviz' C-m
+    tmux send-keys -t tmux_run 'sleep 30; source /opt/ros/melodic/setup.bash && source ~/magni_ws/devel/setup.bash && rviz -d ~/magni_ws/src/magni/magni/config/romio.rviz' C-m
     tmux select-layout tiled
 
     # safety_layer
     tmux split-window -v -t tmux_run
-    tmux send-keys -t tmux_run 'sleep 30; source ~/magni_ws/devel/setup.bash && roslaunch safety_layer safety_layer.launch' C-m
+    tmux send-keys -t tmux_run 'sleep 40; source ~/magni_ws/devel/setup.bash && roslaunch safety_layer safety_layer.launch' C-m
     tmux select-layout tiled
    
     # Hybrid
     tmux split-window -h -t tmux_run
-    tmux send-keys -t tmux_run 'sleep 20; source ~/magni_ws/devel/setup.bash && rosrun rosrun hybrid_mode hybrid_nav_node.py' C-m
-    tmux select-layout tiled
-
-    # docking vision
-    # tmux split-window -h -t tmux_run
-    # tmux send-keys -t tmux_run 'sleep 35; start_docking_vision' C-m
-    # tmux select-layout tiled
-
-    # # docking service server
-    # tmux split-window -h -t tmux_run
-    # tmux send-keys -t tmux_run 'sleep 36; start_docking_server' C-m
-    # tmux select-layout tiled
-
-    # # free fleet
-    # tmux split-window -h -t tmux_run
-    # tmux send-keys -t tmux_run 'sleep 37; start_free_fleet' C-m
-    # tmux select-layout tiled
-
-    # line following
-    #tmux split-window -h -t tmux_run
-    #tmux send-keys -t tmux_run 'sleep 14; roslaunch task_handler_line_following start.launch robotname:=magni' C-m
-    #tmux select-layout tiled
-
-    # line following
-    # tmux split-window -h -t tmux_run
-    # tmux send-keys -t tmux_run 'sleep 12; roslaunch task_handler_pursuit start.launch robotname:=sml' C-m
-    # tmux select-layout tiled
-    
+    tmux send-keys -t tmux_run 'sleep 30; hybrid_nav_core' C-m
+    tmux select-layout tiled    
 
 fi
 tmux attach -t tmux_run
